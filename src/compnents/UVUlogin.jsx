8b865f928372ca1c2b604bc5bf155a4e0724dec3
Nav Volcy <navvolcy@gmail.com>
2024-04-20 19:17:54 -0600

    fixed tailwind issue and finished the frontend design of the login page

--- src/compnents/UVUlogin.jsx
+++ src/compnents/UVUlogin.jsx
@@ -7,31 +7,33 @@ const UVUlogin = () =>{ //doe for the functionaality of the page.
     
     return( 
         <div className="container">
-            <div className="login-form">
+            <div className='page details'>
                 <form>
-                    <div className="img-container">
-                        <img className= 'h-screen flex items-center justify-center' alt="Logged " src={UVU}/>
-                        
+                    <div className='logo max-w-screen-lg mx-auto pb-10'>
+                        <img className='flex items-center' alt="Logo " src={UVU}/>
                     </div>
-                    <div className="username_password_container">
-                        <div>
-                            <label><b>Username</b></label>
+                        
+                        
+                   
+                    <div>
+                        <div className=' p-4' >
+                            <label className=' p-4' ><b>Username</b></label>
                             <input/> 
                         </div>
-                        <div>
-                            <label><b>Password</b></label>
+                        <div >
+                            <label className=' p-6'><b>Password</b></label>
                             <input/>  
                         </div>
                         <div>
-                            <button className='w-3/5 h-10 justify-center block text-white text-[1em] font-[bold] transition-[0.2s] duration-[ease-in] cursor-pointer mt-5 mx-auto my-2.5 rounded-[5px] border-[none]'>login</button>
+                            <button className=' bg-[#06110d] text-[white] cursor-pointer w-full mx-0 my-2 px-5 py-3.5 border-[none]'>login</button>
                         </div>
                         <div>
-                            <input className= 'mx-[120px] my-2.5' type='checkbox' />
-                            <label >Administator</label>
-                            <input type='checkbox'/>
-                            <label>Teacher</label>
-                            <input type='checkbox'/>
-                            <label>TA</label>
+                            <input className= 'mx-[15px]' type='checkbox' />
+                            <label><b>Administator</b></label>
+                            <input  className= 'mx-[15px]' type='checkbox'/>
+                            <label><b>Teacher</b></label>
+                            <input className= 'mx-[15px]' type='checkbox'/>
+                            <label><b>TA</b></label>
                         </div>
                     </div>
 
