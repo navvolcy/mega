5c57b51c39bddbdd4ba75a1499cdafdb8a8bb75b
Nav Volcy <navvolcy@gmail.com>
2024-04-26 20:32:15 -0600

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { Admin, Resource} from 'react-admin';
- 
 import restProvider from 'ra-data-simple-rest';
 import './App.css';
-import UVUlogin from "./compnents/UVUlogin";
+//import UVUlogin from "./compnents/UVUlogin";
 //Use client side routing for navigating between pages.
 //Use a state store.
 //You must have automated testing.
+//The <Resource> component is a configuration component that allows to define sub components for each of the admin view: list, edit, and create
 
-export const App = () =>(
-  // <UVUlogin/>,
-  <Admin dataProvider={restProvider('http://localhost:3000')}>
+// Admin can create and see all courses and all student logs. Admin can create a teacher, TA, or student. Admin is "god" of the system.
+// Create/delet/view  Teachers, TA, and students -> 
+// Create/delete/view courses & log 
+// Each one will have a card that can be edited and links to the db  is Teacher, TA, and Student
+function App () {
+  return (
+    // <UVUlogin/>,
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+      <Resource/>
+    </Admin>
+  );
+}
 
-  </Admin>
-);
+export default App;
